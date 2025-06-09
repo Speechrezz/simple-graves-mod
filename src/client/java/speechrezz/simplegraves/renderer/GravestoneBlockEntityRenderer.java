6fe11fb286ec4f38e4e698ea22236881d828bc7c
Speechrezz <thespeechlessmusic@gmail.com>
2025-06-09 12:15:46 -0700

    Added gravestone text offset based on facing direciton

--- src/client/java/speechrezz/simplegraves/renderer/GravestoneBlockEntityRenderer.java
+++ src/client/java/speechrezz/simplegraves/renderer/GravestoneBlockEntityRenderer.java
@@ -2,12 +2,15 @@ package speechrezz.simplegraves.renderer;
 
 import com.mojang.authlib.GameProfile;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.util.math.Vec3d;
 import speechrezz.simplegraves.entity.custom.GravestoneBlockEntity;
@@ -16,35 +19,50 @@ public class GravestoneBlockEntityRenderer implements BlockEntityRenderer<Graves
 
     @Override
     public void render(GravestoneBlockEntity entity, float tickProgress, MatrixStack matrices,
-            VertexConsumerProvider vertexConsumers, int light, int overlay, Vec3d cameraPos) {
-        
-            MinecraftClient client = MinecraftClient.getInstance();
-            if (client.player == null) return;
+                       VertexConsumerProvider vertexConsumers, int light, int overlay, Vec3d cameraPos) {
 
-            GameProfile profile = entity.getGraveOwner();
-            if (profile == null) return;
-            TextRenderer textRenderer = client.textRenderer;
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player == null) return;
 
-            matrices.push();
-            matrices.translate(0.5, 1.5, 0.5);
+        GameProfile profile = entity.getGraveOwner();
+        if (profile == null) return;
+        TextRenderer textRenderer = client.textRenderer;
 
-            float yaw = client.gameRenderer.getCamera().getYaw();
-            float pitch = client.gameRenderer.getCamera().getPitch();
-            
-            // Rotate to face player
-            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-yaw));
-            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(pitch));
-            
-            matrices.scale(-0.025f, -0.025f, 0.025f);
+        Direction direction = entity.getCachedState().get(Properties.HORIZONTAL_FACING);
+        matrices.push();
+        double directionOffset = 0.3;
+        switch (direction) {
+            case NORTH:
+                matrices.translate(0.5, 1.4, 0.5 + directionOffset);
+                break;
+            case SOUTH:
+                matrices.translate(0.5, 1.4, 0.5 - directionOffset);
+                break;
+            case EAST:
+                matrices.translate(0.5 - directionOffset, 1.4, 0.5);
+                break;
+            case WEST:
+                matrices.translate(0.5 + directionOffset, 1.4, 0.5);
+                break;
+        }
 
-            Text nameText = Text.literal(profile.getName());
-            int textWidth = textRenderer.getWidth(nameText);
+        float yaw = client.gameRenderer.getCamera().getYaw();
+        float pitch = client.gameRenderer.getCamera().getPitch();
 
-            textRenderer.draw(nameText, -textWidth / 2.0f, 0, 0xFFFFFF, false, 
-                            matrices.peek().getPositionMatrix(), vertexConsumers, 
-                            TextRenderer.TextLayerType.NORMAL, 0, light);
+        // Rotate to face player
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-yaw));
+        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(pitch));
 
-            matrices.pop();
+        matrices.scale(-0.025f, -0.025f, 0.025f);
+
+        Text nameText = Text.literal(profile.getName());
+        int textWidth = textRenderer.getWidth(nameText);
+
+        textRenderer.draw(nameText, -textWidth / 2.0f, 0, 0xFFFFFF, false,
+                matrices.peek().getPositionMatrix(), vertexConsumers,
+                TextRenderer.TextLayerType.NORMAL, 0, light);
+
+        matrices.pop();
     }
-    
+
 }
