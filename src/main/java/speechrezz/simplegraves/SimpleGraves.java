2338143fe6d9b1f1c12ba0c7f8bc9089cfb37889
Speechrezz <thespeechlessmusic@gmail.com>
2025-06-09 16:25:43 -0700

    Moved functions out of mixin and into the GraveStone file

--- src/main/java/speechrezz/simplegraves/SimpleGraves.java
+++ src/main/java/speechrezz/simplegraves/SimpleGraves.java
@@ -2,26 +2,89 @@ package speechrezz.simplegraves;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.Properties;
+import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import speechrezz.simplegraves.block.ModBlocks;
 import speechrezz.simplegraves.entity.ModBlockEntities;
+import speechrezz.simplegraves.entity.custom.GravestoneBlockEntity;
+
+import java.util.Optional;
 
 public class SimpleGraves implements ModInitializer {
 	public static final String MOD_ID = "simplegraves";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		ModBlocks.initialize();
 		ModBlockEntities.initialize();
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Simple Graves Initialized");
+	}
+
+	public static boolean placeGravestone(World world, PlayerInventory inventory) {
+		Optional<BlockPos> gravePosOptional = findGravestonePlacementPosition(world, inventory.player.getBlockPos());
+		if (gravePosOptional.isEmpty()) return false;
+		BlockPos gravePos = gravePosOptional.get();
+
+		// Place gravestone
+		BlockState graveState = ModBlocks.GRAVESTONE.getDefaultState()
+				.with(Properties.HORIZONTAL_FACING, inventory.player.getHorizontalFacing().getOpposite())
+				.with(Properties.WATERLOGGED, world.getFluidState(gravePos).isOf(Fluids.WATER));
+		if (!world.setBlockState(gravePos, graveState)) return false;
+		tryPlaceDirt(world, gravePos.down(1));
+		tryPlaceDirt(world, gravePos.down(1).offset(inventory.player.getHorizontalFacing().getOpposite(), 1));
+
+		// Save player inventory, XP, etc.
+		GravestoneBlockEntity gravestoneBlockEntity = new GravestoneBlockEntity(gravePos, graveState);
+		DefaultedList<ItemStack> graveItems = DefaultedList.of();
+		for (ItemStack stack : inventory)
+			graveItems.add(stack);
+
+		gravestoneBlockEntity.setItems(graveItems);
+		gravestoneBlockEntity.setGraveOwner(inventory.player.getGameProfile());
+		gravestoneBlockEntity.setXp(inventory.player.totalExperience);
+		gravestoneBlockEntity.markDirty();
+		world.addBlockEntity(gravestoneBlockEntity);
+
+		inventory.player.sendMessage(Text.translatable("text.gravestones.grave_coordinates", gravePos.getX(), gravePos.getY(), gravePos.getZ()), false);
+		SimpleGraves.LOGGER.info("[SimpleGraves] Gravestone placed at ({})", gravePos.toShortString());
+		return true;
+	}
+
+	private static boolean canPlaceGravestone(World world, BlockPos blockPos) {
+		if(world.getBlockEntity(blockPos) != null) return false;
+		Block block = world.getBlockState(blockPos).getBlock();
+
+		boolean isWithinWorldBounds = blockPos.getY() <= world.getDimension().height() - world.getDimension().minY();
+		boolean isAir = block == Blocks.AIR || block == Blocks.WATER;
+
+		return isWithinWorldBounds && isAir;
+	}
+
+	private static Optional<BlockPos> findGravestonePlacementPosition(World world, BlockPos startPos) {
+		startPos = startPos.withY(Math.max(world.getDimension().minY() + 1, startPos.getY())); // Clamp Y
+
+		for (BlockPos blockPos : BlockPos.iterateOutwards(startPos, 5, 6, 5))
+			if (canPlaceGravestone(world, blockPos))
+				return Optional.of(blockPos);
+
+		return Optional.empty();
+	}
+
+	private static void tryPlaceDirt(World world, BlockPos dirtPos) {
+		Block block = world.getBlockState(dirtPos).getBlock();
+		if (block == Blocks.AIR || block == Blocks.GRASS_BLOCK)
+			world.setBlockState(dirtPos, Blocks.DIRT.getDefaultState());
 	}
 }
--- src/main/java/speechrezz/simplegraves/mixin/LivingEntityMixin.java
+++ src/main/java/speechrezz/simplegraves/mixin/LivingEntityMixin.java
@@ -1,35 +1,17 @@
 package speechrezz.simplegraves.mixin;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.item.ItemStack;
-import net.minecraft.state.property.Properties;
-import net.minecraft.text.Text;
-import net.minecraft.util.collection.DefaultedList;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import speechrezz.simplegraves.SimpleGraves;
-import speechrezz.simplegraves.block.ModBlocks;
-import speechrezz.simplegraves.entity.custom.GravestoneBlockEntity;
-
-import java.util.Optional;
 
 @Mixin(PlayerEntity.class)
 public abstract class LivingEntityMixin extends LivingEntity {
-	@Shadow @Final private PlayerInventory inventory;
-
 	protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
 	}
@@ -39,7 +21,7 @@ public abstract class LivingEntityMixin extends LivingEntity {
 		World world = this.getWorld();
 		if (world.isClient) return;
 
-		if (placeGravestone(world, inventory)) {
+		if (SimpleGraves.placeGravestone(world, inventory)) {
 			// Clear player inventory, XP, etc. so they don't drop anything.
 			inventory.clear();
 			inventory.player.experienceLevel = 0;
@@ -49,65 +31,5 @@ public abstract class LivingEntityMixin extends LivingEntity {
 		else {
 			inventory.dropAll();
 		}
-	}
-
-	@Unique
-	private static boolean placeGravestone(World world, PlayerInventory inventory) {
-		Optional<BlockPos> gravePosOptional = findGravestonePlacementPosition(world, inventory.player.getBlockPos());
-		if (gravePosOptional.isEmpty()) return false;
-		BlockPos gravePos = gravePosOptional.get();
-
-		// Place gravestone
-		BlockState graveState = ModBlocks.GRAVESTONE.getDefaultState()
-				.with(Properties.HORIZONTAL_FACING, inventory.player.getHorizontalFacing().getOpposite())
-				.with(Properties.WATERLOGGED, world.getFluidState(gravePos).isOf(Fluids.WATER));
-		if (!world.setBlockState(gravePos, graveState)) return false;
-		tryPlaceDirt(world, gravePos.down(1));
-		tryPlaceDirt(world, gravePos.down(1).offset(inventory.player.getHorizontalFacing().getOpposite(), 1));
-
-		// Save player inventory, XP, etc.
-		GravestoneBlockEntity gravestoneBlockEntity = new GravestoneBlockEntity(gravePos, graveState);
-		DefaultedList<ItemStack> graveItems = DefaultedList.of();
-		for (ItemStack stack : inventory)
-			graveItems.add(stack);
-
-		gravestoneBlockEntity.setItems(graveItems);
-		gravestoneBlockEntity.setGraveOwner(inventory.player.getGameProfile());
-		gravestoneBlockEntity.setXp(inventory.player.totalExperience);
-		gravestoneBlockEntity.markDirty();
-		world.addBlockEntity(gravestoneBlockEntity);
-
-		inventory.player.sendMessage(Text.translatable("text.gravestones.grave_coordinates", gravePos.getX(), gravePos.getY(), gravePos.getZ()), false);
-		SimpleGraves.LOGGER.info("[SimpleGraves] Gravestone placed at ({})", gravePos.toShortString());
-		return true;
-	}
-
-	@Unique
-	private static boolean canPlaceGravestone(World world, BlockPos blockPos) {
-		if(world.getBlockEntity(blockPos) != null) return false;
-		Block block = world.getBlockState(blockPos).getBlock();
-
-		boolean isWithinWorldBounds = blockPos.getY() <= world.getDimension().height() - world.getDimension().minY();
-		boolean isAir = block == Blocks.AIR || block == Blocks.WATER;
-
-		return isWithinWorldBounds && isAir;
-	}
-
-	@Unique
-	private static Optional<BlockPos> findGravestonePlacementPosition(World world, BlockPos startPos) {
-		startPos = startPos.withY(Math.max(world.getDimension().minY() + 1, startPos.getY())); // Clamp Y
-
-		for (BlockPos blockPos : BlockPos.iterateOutwards(startPos, 5, 6, 5))
-			if (canPlaceGravestone(world, blockPos))
-				return Optional.of(blockPos);
-
-		return Optional.empty();
-	}
-
-	@Unique
-	private static void tryPlaceDirt(World world, BlockPos dirtPos) {
-		Block block = world.getBlockState(dirtPos).getBlock();
-		if (block == Blocks.AIR || block == Blocks.GRASS_BLOCK)
-			world.setBlockState(dirtPos, Blocks.DIRT.getDefaultState());
 	}
 }
